0a6195e07c492b7e23ce02a1e7a25af29d92f428
licius22-cmd <licius22@gmail.com>
2026-02-13 03:11:40 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Instalando dependências (se necessário)..."
-python3 -m 
+python3 -m flask run --host=0.0.0.0 --port=5001
 
 echo "Iniciando servidor Audio Criativo..."
 echo "Acesse http://0.0.0.0:5001 no seu navegador"
