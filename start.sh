590d2cf8e0a9ce3e320e247658b8387b385eea77
licius22-cmd <licius22@gmail.com>
2026-02-13 02:46:49 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ echo "Instalando dependências (se necessário)..."
 python3 -m pip install -r requirements.txt
 
 echo "Iniciando servidor Audio Criativo..."
-echo "Acesse http://127.0.0.1:5001 no seu navegador"
+echo "Acesse http://0.0.0.0:5001 no seu navegador"
 python3 app.py
